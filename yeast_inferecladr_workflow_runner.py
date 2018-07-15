1997c11b7ab305187e61c12fb9b7283377276d38
Kostya HPC <kmt331@nyu.edu>
2018-07-15 01:04:45 -0400

    removed redundancy with creating an extra class in yeast_inferecladr_workflow, which added the prior-splitting class, which is already added in the inferecladr class in the first place

--- yeast_inferecladr_workflow_runner.py
+++ yeast_inferecladr_workflow_runner.py
@@ -2,21 +2,21 @@ from inferelator_ng.bbsr_tfa_workflow import BBSR_TFA_Workflow
 from inferelator_ng.prior_gs_split_workflow import PriorGoldStandardSplitWorkflowBase
 from inferelator_ng.inferecladr_workflow import InfereCLaDR_Workflow
 
-class InfereCLaDR_Workflow_with_Prior_GS_split(InfereCLaDR_Workflow, PriorGoldStandardSplitWorkflowBase):
-    """ 
-        The class BBSR_TFA_Workflow_with_Prior_GS_split is a case of multiple inheritance,
-        as it inherits both from BBSR_TFA_Workflow and PriorGoldStandardSplitWorkflowBase      
-    """
 
-workflow = InfereCLaDR_Workflow_with_Prior_GS_split()
+workflow = InfereCLaDR_Workflow()
 # Common configuration parameters
 workflow.input_dir = 'data/yeast'
-#workflow.expression_matrix_file = "clusters/expr_clust1.tsv"
-#workflow.meta_data_file = "clusters/meta_clust1.tsv"
+#workflow.expr_clust_files = ["clusters/expr_clust1.tsv", "clusters/expr_clust2.tsv", "clusters/expr_clust3.tsv", "clusters/expr_clust4.tsv"]
+workflow.expr_clust_files = ["clusters/expr_clust2.tsv", "clusters/expr_clust3.tsv"]
+#workflow.meta_clust_files = ["clusters/meta_clust1.tsv", "clusters/meta_clust2.tsv", "clusters/meta_clust3.tsv", "clusters/meta_clust4.tsv"]
+workflow.meta_clust_files = ["clusters/meta_clust2.tsv", "clusters/meta_clust3.tsv"]
 workflow.num_bootstraps = 1
 workflow.delTmax = 6000
-workflow.delTmin = 0
-#workflow.tau = 45
+workflow.delTmin = 1
+#workflow.seeds = range(42,62)
+workflow.seeds = range(42,44)
+#workflow.taus = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 200, 250]
+workflow.taus = [0, 5, 10]
 #workflow.run() 
 workflow.reduce_searchspace=True
 workflow.optimize_taus()
